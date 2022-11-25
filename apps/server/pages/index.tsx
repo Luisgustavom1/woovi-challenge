eabc50948fab73293a2c3d22dae70b9a8855ffde
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-11-24 21:45:29 -0300

    rename docs to server

--- apps/server/pages/index.tsx
+++ apps/server/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from 'ui';
 
-export default function Docs() {
+export default function Server() {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>Server</h1>
       <Button />
     </div>
   );
